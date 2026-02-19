c4924918bafc6c3a4028694094cec70413530968
Sean Dorsett <sedorset@microsoft.com>
2026-02-18 22:52:33 -0800

    Remove Playwright configuration file

--- playwright.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig, devices } from '@playwright/test';
-
-export default defineConfig({
-  projects: [
-    { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
-    { name: 'firefox',  use: { ...devices['Desktop Firefox'] } },
-    { name: 'webkit',   use: { ...devices['Desktop Safari'] } },
-  ],
-});
